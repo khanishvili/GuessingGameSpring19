ebc921f256fd202e48c49e1454106bd0233fc127
khanishvili <khnshvl@gmail.com>
2019-01-16 08:26:40 -0800

    Added Goodbyee

--- GuessingGameSpring19/Program.cs
+++ GuessingGameSpring19/Program.cs
@@ -7,6 +7,7 @@ namespace GuessingGameSpring19
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Goobye ");
         }
     }
 }

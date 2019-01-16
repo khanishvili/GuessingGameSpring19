6c44d4152525187b09b891a2654a8ef83e8b42c2
khanishvili <khnshvl@gmail.com>
2019-01-16 11:12:10 -0800

    added help option

--- GuessingGameSpring19/Program.cs
+++ GuessingGameSpring19/Program.cs
@@ -4,10 +4,26 @@ namespace GuessingGameSpring19
 {
     class Program
     {
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("adios ");
+            Console.WriteLine("PLease gues a Number between 1 to 10 or 0 for Help");
+            string userguess = Console.ReadLine();
+            if (userguess.Equals("0"))
+            {
+                Console.WriteLine("=========Instructions=========");
+                Console.WriteLine("Gueas a Number between 1 to 10");
+                Console.WriteLine("hit enter to submit your guess");
+            }
+            else if (userguess.Equals("7")) 
+                {
+                Console.WriteLine("u won the game");
+            }
+            else
+            {
+                Console.WriteLine("You lost");
+            }
+
         }
-    }
-}
+    } }
